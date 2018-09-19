6f90d14d76e3e06b38304b133b7394ab6ee7ae23
samyamshrestha <samyamsth@gmail.com>
2018-09-19 00:24:13 -0400

    Added activity classes to AndroidManifest.xml

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -1,4 +1,13 @@
 package com.example.android.miwok;
 
-public class PhrasesActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class PhrasesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phrases);
+    }
 }
